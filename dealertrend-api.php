20c371f22e0ada73d335649a7650633430652bbb
Sterling Hamilton <sterling.hamilton+git-commit@gmail.com>
2011-01-12 21:16:31 -0800

    Adding PHP 4 constructor. Adding method for grabbing plugin meta data.

--- dealertrend-api.php
+++ dealertrend-api.php
@@ -7,6 +7,35 @@
 if ( !class_exists( 'dealertrend_api') ) {
 
   class dealertrend_api {
+
+    public $plugin_meta_data = array();
+
+    # PHP 4 Constructor
+    function dealertrend_api() {
+
+      # Retrieve information about the plugin - the WordPress way.
+      $this->load_plugin_meta_data();
+
+    }
+
+    function load_plugin_meta_data() {
+    
+      # Specify what headers we are looking for
+      $default_headers = array (
+        'Name' => 'Plugin Name',
+        'PluginURI' => 'Plugin URI',
+        'Version' => 'Version',
+        'Description' => 'Description',
+        'Author' => 'Author',
+      );
+  
+      # Use those headers and parse our plugins meta data.
+      $this->plugin_meta_data = get_file_data( __FILE__ , $default_headers , 'plugin' );
+      $this->plugin_meta_data[ 'BaseURL' ] = WP_PLUGIN_URL . '/' . basename( dirname( __FILE__ ) );
+
+    } 
+
+
   } # End class definition.
 
 } # Does the class exist?
