0199a836d8fc7e036f6e8084d38c0b64271bf735
Sterling Hamilton <sterling.hamilton+git-commit@gmail.com>
2011-01-12 21:12:14 -0800

    Creating class and having it executed on startup.

--- dealertrend-api.php
+++ dealertrend-api.php
@@ -3,4 +3,16 @@
  * Plugin Name: DealerTrend API Plugin
  * Version: 1.0.0
  */
+
+if ( !class_exists( 'dealertrend_api') ) {
+
+  class dealertrend_api {
+  } # End class definition.
+
+} # Does the class exist?
+
+if ( class_exists( 'dealertrend_api' ) and !isset( $dealertrend_api ) ) {
+  $dealertrend_api = new dealertrend_api();
+}
+
 ?>
