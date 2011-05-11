67e55a27948b90dddf43f4541e311d6ee0deafeb
Sterling Hamilton <sterling.hamilton+git-commit@gmail.com>
2011-05-11 14:16:34 -0700

    Making bobcat stable again.

--- app/views/inventory/bobcat/index.php
+++ app/views/inventory/bobcat/index.php
@@ -2,10 +2,15 @@
 
 	global $wp_rewrite;
 
+	wp_enqueue_script( 'jquery' );
+	wp_enqueue_script( 'jquery-ui' );
+	wp_enqueue_script( 'jquery-ui-tabs' );
+	wp_enqueue_script( 'jquery-ui-cycle' );
+
 	wp_enqueue_script(
-		'dealertrend-inventory-theme-default',
-		$this->meta_information[ 'PluginURL' ] . '/app/views/inventory/default/js/slideshow.js',
-		array( 'jquery' , 'jquery-ui-core' , 'jquery-ui-tabs' , 'jquery-cycle', 'dealertrend-inventory-api' ),
+		'dealertrend-inventory-theme-bobcat-slideshow',
+		$this->meta_information[ 'PluginURL' ] . '/app/views/inventory/bobcat/js/slideshow.js',
+		array( 'jquery-cycle', 'dealertrend-inventory-api' ),
 		$this->meta_information[ 'Version' ],
 		true
 	);
@@ -14,53 +19,80 @@
 	get_header();
 	flush();
 
-	if( $vehicle_management_system->check_host() == false ) {
+	$check_host = $vehicle_management_system->check_host();
+	if( $check_host[ 'status' ] == false ) {
 		echo '<h2 style="font-family:Helvetica,Arial; color:red;">Unable to display inventory. Please contact technical support.</h2><br class="clear" />';
 		echo '<p>Unable to connect to API.</p>';
 		return false;
 	}
 
-	if( $vehicle_management_system->check_company_id() == false ) {
+	$check_company_id = $vehicle_management_system->check_company_id();
+	if( $check_company_id[ 'status' ] == false ) {
 		echo '<h2 style="font-family:Helvetica,Arial; color:red;">Unable to display inventory. Please contact technical support.</h2><br class="clear" />';
 		echo '<p>Unable to validate company information.</p>';
 		return false;
 	}
 
-	if( $vehicle_management_system->check_inventory() == false ) {
+	$check_inventory = $vehicle_management_system->check_inventory();
+
+	if( $check_inventory[ 'status' ] == false && $check_inventory[ 'code' ] != 200 ) {
 		echo '<h2 style="font-family:Helvetica,Arial; color:red;">Unable to display inventory. Please contact technical support.</h2><br class="clear" />';
 		echo '<p>Unable to retrieve inventory.</p>';
 		return false;
 	}
 
-	$company_information = $vehicle_management_system->get_company_information();
-	$state = $company_information->state;
-	$city = $company_information->city;
-	$company_name = strtoupper( $company_information->name );
+	$inventory = $vehicle_management_system->get_inventory( $this->parameters );
 
-	$breadcrumbs = '<a href="/" title="' . $company_name . ': Home Page">' . urldecode( $company_name ) . '</a>';
-	$do_not_show = array( 'page' , 'per_page', 'trim', 'body_style', 'vehicleclass', 'sort', 'city', 'state' );
-
-	if( count( $this->parameters > 1 ) ) {
-		$crumb_trail = '/inventory/';
-		if( !empty( $wp_rewrite->rules ) ) {
-			foreach( $this->parameters as $key => $value ) {
-				if( !in_array( $key ,$do_not_show ) && $key != 'taxonomy' ) {
-					$crumb_trail .= $value . '/';
-					$breadcrumbs .= ' > <a href=' . $crumb_trail . '>' . ucfirst( urldecode( $value ) ) . '</a>';
-				} 
-			} 
-		} else {
-			$crumb_trail = '?taxonomy=inventory';
-			foreach( $this->parameters as $key => $value ) {
-				if( !in_array( $key ,$do_not_show ) && $key != 'taxonomy' ) {
-					$crumb_trail .= '&amp;' . $key . '=' . $value;
-					$breadcrumbs .= ' > <a href=' . $crumb_trail . '>' . ucfirst( urldecode( $value ) ) . '</a>';
-				} 
-			} 
-		} 
+	if( $inventory === false ) {
+		echo '<h2 style="font-family:Helvetica,Arial; color:red;">Unable to display inventory. Please contact technical support.</h2><br class="clear" />';
+		echo '<p>The inventory feed timed out while trying to display. Please refresh the page. If the feed refuses to return data, then the given parameters may be invalid.</p>';
+		return false;
 	}
 
-	$breadcrumbs = '<div class="breadcrumbs">' . $breadcrumbs . '</div>';
+	$company_information = $vehicle_management_system->get_company_information();
+	$company_information = $company_information[ 'data' ];
+	$state = $company_information->state;
+	$city = $company_information->city;
+
+	$company_name = strtoupper( $company_information->name );
+
+	$parameters = $this->parameters;
+
+	$breadcrumbs = '<a href="/" title="' . $company_name . ': Home Page">' . urldecode( $company_name ) . '</a>';
+	$do_not_show = array( 'page' , 'per_page' , 'trim' , 'body_style' , 'vehicleclass' , 'sort' , 'city' , 'state' , 'search', 'price_from' , 'price_to' , 'certified' );
+
+	unset( $parameters[ 'taxonomy' ] );
+
+	if( !isset( $parameters[ 'saleclass' ] ) ){
+		if( isset( $inventory->saleclass ) ) {
+			$substitute = $inventory->saleclass;
+			array_shift( $parameters );
+			$substitute_array = array( 'saleclass' => $substitute );
+			$parameters = $substitute_array + $parameters;
+		}
+	}
+
+	if( count( $parameters > 1 ) ) {
+		$crumb_trail = '/inventory/';
+		if( !empty( $wp_rewrite->rules ) ) {
+			foreach( $parameters as $key => $value ) {
+				if( !in_array( $key ,$do_not_show ) ) {
+					$crumb_trail .= $value . '/';
+					$breadcrumbs .= '<a href=' . $crumb_trail . '> > ' . ucfirst( urldecode( $value ) ) . '</a>';
+				}
+			}
+		} else {
+			$crumb_trail = '?taxonomy=inventory';
+			foreach( $parameters as $key => $value ) {
+				if( !in_array( $key ,$do_not_show ) ) {
+					$crumb_trail .= '&amp;' . $key . '=' . $value;
+					$breadcrumbs .= '<a href=' . $crumb_trail . '> > ' . ucfirst( urldecode( $value ) ) . '</a>';
+				}
+			}
+		}
+	}
+
+	$breadcrumbs = '<div class="bobcat-breadcrumbs">' . $breadcrumbs . '</div>';
 
 	$inventory = $vehicle_management_system->get_inventory( $this->parameters );
 
