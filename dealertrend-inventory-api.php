5b67244f28752ac5e8cf0e54c2a4ae02ded6ee16
Sterling Hamilton <sterling.hamilton+git-commit@gmail.com>
2011-05-16 13:28:36 -0700

    Bumping version to 3.0.1

--- dealertrend-inventory-api.php
+++ dealertrend-inventory-api.php
@@ -5,7 +5,7 @@
  * Author: DealerTrend, Inc.
  * Author URI: http://www.dealertrend.com
  * Description: Provides access to the Vehicle Management System provided by <a href="http://www.dealertrend.com" target="_blank" title="DealerTrend, Inc: Shift Everything">DealerTrend, Inc.</a>
- * Version: 3.0.0
+ * Version: 3.0.1
  */
 
 if ( !class_exists( 'dealertrend_inventory_api' ) ) {
