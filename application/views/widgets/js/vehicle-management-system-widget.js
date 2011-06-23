6026ef3b98a36ddbcf195d1bc27f8a3b5131bdd9
Sterling Hamilton <sterling.hamilton+git-commit@gmail.com>
2011-06-23 16:05:09 -0700

    Simplifying the javascript and css for the widgets.
    Signed-off-by: Sterling Hamilton <sterling.hamilton+git-commit@gmail.com>

--- application/views/widgets/js/vehicle-management-system-widget.js
+++ application/views/widgets/js/vehicle-management-system-widget.js
@@ -1,3 +1,8 @@
 var dealertrend = jQuery.noConflict();
 
-dealertrend("div.vms-widget-content.carousel").carousel( { autoSlide: true , loop: true } );
+dealertrend( document ).ready(
+  function() {
+    dealertrend( '.vms-widget-content' ).tabs();
+    dealertrend( '.vms-widget-content.carousel .vms-widget-content-wrapper' ).carousel( { autoSlide: true , loop: true } );
+  }
+);
