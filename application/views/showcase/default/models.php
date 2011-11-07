22f9408937e6fea28190300c53aa667b1aec8b49
Sterling Hamilton <sterling.hamilton+git-commit@gmail.com>
2011-11-07 10:06:34 -0800

    Altering structure slightly for aesthetics and allows for stored value references.

--- application/views/showcase/default/models.php
+++ application/views/showcase/default/models.php
@@ -28,6 +28,8 @@
 	$models = $model_data;
 	usort( $models , 'sort_models' );
 
+	$selected_models = $this->options[ 'vehicle_reference_system' ][ 'data' ][ 'models' ];
+
 	$classifications = array(
 		'T' => 'Trucks', 
 		'M' => 'Medium Duty Vehicles',
@@ -45,20 +47,22 @@
 	echo '<h3>&raquo; ' . $classifications[ $models[0]->classification ] . '</h3>';
 	echo '<div class="items">';
 	foreach( $models as $model ) {
-		if( $model->name != $previous_name && $model->image_urls->small !== NULL ) {
-			$previous_name = $model->name;
-			if( $model->classification != $previous_classification) {
-				$previous_classification = $model->classification;
+		if( in_array( $model->name , $selected_models ) ) {
+			if( $model->name != $previous_name && $model->image_urls->small !== NULL ) {
+				$previous_name = $model->name;
+				if( $model->classification != $previous_classification) {
+					$previous_classification = $model->classification;
+					echo '</div>';
+					echo '<h3>&raquo; ' . $classifications[ $model->classification ] . '</h3>';
+					echo '<div class="items">';
+				}
+				echo '<div class="model">';
+				echo '<a href="/showcase/' . $make . '/' . $model->name . '" title="' . $make . ' ' . $model->name .'">';
+				echo '<img src="' . preg_replace( '/IMG=(.*)\.\w{3,4}/i' , 'IMG=\1.png' , $model->image_urls->small ) . '" />';
+				echo '<div class="name">' . $model->name . '</div>';
+				echo '</a>';
 				echo '</div>';
-				echo '<h3>&raquo; ' . $classifications[ $model->classification ] . '</h3>';
-				echo '<div class="items">';
 			}
-			echo '<div class="model">';
-			echo '<a href="/showcase/' . $make . '/' . $model->name . '" title="' . $make . ' ' . $model->name .'">';
-			echo '<img src="' . preg_replace( '/IMG=(.*)\.\w{3,4}/i' , 'IMG=\1.png' , $model->image_urls->small ) . '" />';
-			echo '<div class="name">' . $model->name . '</div>';
-			echo '</a>';
-			echo '</div>';
 		}
 	}
 	echo '</div>';
