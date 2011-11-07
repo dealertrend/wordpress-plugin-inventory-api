a9c6c003300089dcfa6fe36ac03e941709aa4a3c
Sterling Hamilton <sterling.hamilton+git-commit@gmail.com>
2011-11-07 10:04:20 -0800

    Removing non-standard JS.
    console.log breaks in IE.

--- application/views/widgets/js/vehicle-reference-system-admin.js
+++ application/views/widgets/js/vehicle-reference-system-admin.js
@@ -2,22 +2,16 @@ var dealertrend_admin = jQuery.noConflict();
 
 function implement_ui() {
 
-    console.log( 'vrs-widget: applying ui styles/scripts' );
-
     dealertrend_admin( 'select[multiple]="multiple"' ).each( function() {
         var label;
         var classes;
         var instance =  dealertrend_admin( this );
 
-        console.log( 'vrs-widget: ' + instance.attr( 'id' ) + ' -> ' + instance.attr( 'class' ) + ' -> starting' );
-
         if( instance.has( 'button' ).length ) {
-            console.log( 'vrs-widget: aborting! already styled' );
             return true;
         }
 
         if( instance.attr( 'name' ) == 'widget-vehicle_reference_system_widget[__i__][makes][]' ) {
-            console.log( 'vrs-widget: aborting! not an active widget' );
             return true;
         }
 
@@ -28,7 +22,6 @@ function implement_ui() {
             label = 'Select a Model';
             classes = 'models';
         } else {
-            console.log( 'vrs-widget: what do i do with this?' );
             return true;
         }
 
@@ -40,15 +33,12 @@ function implement_ui() {
         } )
         .multiselectfilter();
 
-        console.log( 'vrs-widget: ' + instance.attr( 'id' ) + ' -> ' + instance.attr( 'class' ) + ' -> finished' );
-
     } );
 }
 
 dealertrend_admin( document ).ready( function() {
     implement_ui();
 } );
-
 
 dealertrend_admin( 'body' ).ajaxSuccess( function( evt, request, settings ) {
     if( typeof( settings.data ) !== 'undefined' ) {
