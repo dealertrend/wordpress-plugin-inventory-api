b79a5c6b4310e3c293f7a516947ee186f55943b2
Sterling Hamilton <sterling.hamilton+git-commit@gmail.com>
2011-11-07 10:05:57 -0800

    Implementing stored options reference.

--- application/views/showcase/default/makes.php
+++ application/views/showcase/default/makes.php
@@ -8,20 +8,24 @@
 
 	$makes = array_merge( $make_data[ $next_year ] , $make_data[ $current_year ] , $make_data[ $last_year ] );
 
+	$selected_makes = $this->options[ 'vehicle_reference_system' ][ 'data' ][ 'makes' ];
+
 	$bucket = array();
 
 	echo '<h2><a href="/showcase/">Showcase</a> &rsaquo; All Makes</h2>';
 	echo '<hr />';
 	echo '<div class="group makes">';
-	foreach( $makes as $make ) { 
-		if( ! in_array( $make->id , $bucket ) ) {
-			$bucket[] = $make->id;
-			if( ! empty( $make->image_url ) ) { 
-				echo '<div class="make">';
-				echo '<a href="/showcase/' . $make->name . '/" title="' . $make->name . '"><img src="' . $make->image_url . '" /></a>';
-				echo '<div class="classifications">';
-				echo '</div>';
-				echo '</div>';
+	foreach( $makes as $make ) {
+		if( in_array( $make->name , $selected_makes ) ) {
+			if( ! in_array( $make->id , $bucket ) ) {
+				$bucket[] = $make->id;
+				if( ! empty( $make->image_url ) ) { 
+					echo '<div class="make">';
+					echo '<a href="/showcase/' . $make->name . '/" title="' . $make->name . '"><img src="' . $make->image_url . '" /></a>';
+					echo '<div class="classifications">';
+					echo '</div>';
+					echo '</div>';
+				}
 			}
 		}
 	}
