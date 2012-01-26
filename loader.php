2c51be798fbee0e0227ed5cc5e80d5f3adc1d90c
Sterling Hamilton <sterling.hamilton+git-commit@gmail.com>
2012-01-26 13:16:28 -0800

    Bumping the version.

--- loader.php
+++ loader.php
@@ -5,7 +5,7 @@
  * Author: DealerTrend, Inc.
  * Author URI: http://www.dealertrend.com
  * Description: Provides access to the Vehicle Management System and Vehicle Reference System provided by <a href="http://www.dealertrend.com" target="_blank" title="DealerTrend, Inc: Shift Everything">DealerTrend, Inc.</a>
- * Version: 3.14.0
+ * Version: 3.15.0
  * License: GPLv2 or later
  */
 
