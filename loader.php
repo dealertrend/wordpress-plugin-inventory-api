0f51f2bb7e1f36b69fc357108e6c80d97f25d7f5
Sterling Hamilton <sterling.hamilton+git-commit@gmail.com>
2012-02-20 14:48:09 -0800

    Updating licensing information.

--- loader.php
+++ loader.php
@@ -7,6 +7,22 @@
  * Description: Provides access to the Vehicle Management System and Vehicle Reference System provided by <a href="http://www.dealertrend.com" target="_blank" title="DealerTrend, Inc: Shift Everything">DealerTrend, Inc.</a>
  * Version: 3.16.1
  * License: GPLv2 or later
+
+   Copyright (C) 2012  DealerTrend, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
  */
 
 function print_me( $content ) {
