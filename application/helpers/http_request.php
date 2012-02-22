e7adb0ef6b6c59936b653cf8bd0a185df2328e8e
Sterling Hamilton <sterling.hamilton+git-commit@gmail.com>
2012-02-22 11:49:00 -0800

    Removing constant, replacing with option.

--- application/helpers/http_request.php
+++ application/helpers/http_request.php
@@ -6,14 +6,11 @@ print_me( __FILE__ );
 
 class http_request {
 
-	const timeout = 20;
-
 	public $url = null;
 
 	public $group = null;
 
 	public $request_parameters = array(
-		'timeout' => http_request::timeout,
 		'headers' => array(
 			'Referer' => NULL,
 			'X-WordPress-Version' => NULL,
@@ -26,10 +23,12 @@ print_me( __METHOD__ );
 		global $wp , $wp_version , $dealertrend_inventory_api;
 		$this->url = $url;
 		$this->group = $group;
+
 		$plugin_options = get_option( 'dealertrend_inventory_api' );
 		$this->request_parameters[ 'headers' ][ 'Referer' ] = site_url() . '/' . $wp->request;
 		$this->request_parameters[ 'headers' ][ 'X-WordPress-Version' ] = $wp_version;
 		$this->request_parameters[ 'headers' ][ 'X-Plugin-Version' ] = $dealertrend_inventory_api->plugin_information[ 'Version' ];
+		$this->request_parameters[ 'timeout'] = ! isset( $this->request_parameters[ 'timeout'] ) ? $dealertrend_inventory_api->options[ 'requests' ][ 'timeout_seconds' ] : $this->request_parameters[ 'timeout'];
 	}
 
 	public function cached() {
