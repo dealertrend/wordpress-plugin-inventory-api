05626d098eb42d099e345cb94166bea7746ac1a4
cloudxero <jorge.chavez@dealertrend.com>
2012-11-06 11:17:55 -0800

    Change: Now checks to make sure json feed is not null before trying to apply fuel economy values.
    Wasn't causing any real issues on the front end, but was throwing up a js script error.

--- application/views/showcase/default/trims.php
+++ application/views/showcase/default/trims.php
@@ -75,13 +75,17 @@ console.log('/dealertrend-ajax/showcase/<?php echo $make; ?>/<?php echo $model; 
               context: document.body,
               success: function(data) {
                 var json = JSON.parse(data);
-<?php if($country_code == 'CA') { ?>
-                dealertrend( '#fuel #city .number' ).html( json[ 0 ].city_lp_100km );
-                dealertrend( '#fuel #hwy .number' ).html( json[ 0 ].highway_lp_100km );
-<?php } else { ?>
-                dealertrend( '#fuel #city .number' ).html( json[ 0 ].city_mpg );
-                dealertrend( '#fuel #hwy .number' ).html( json[ 0 ].highway_mpg );
-<?php } ?>
+		if ( json == '' || json == null ) {
+
+		} else {
+			<?php if($country_code == 'CA') { ?>
+        	       		dealertrend( '#fuel #city .number' ).html( json[ 0 ].city_lp_100km );
+        	       		dealertrend( '#fuel #hwy .number' ).html( json[ 0 ].highway_lp_100km );
+			<?php } else { ?>
+        	        	dealertrend( '#fuel #city .number' ).html( json[ 0 ].city_mpg );
+        	        	dealertrend( '#fuel #hwy .number' ).html( json[ 0 ].highway_mpg );
+			<?php } ?>
+		}
                 button.css('cursor','pointer');
               }
             });
