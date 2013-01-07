b103ba2275441e88f879da3aa3c9d4c94a417c6a
cloudxero <jorge.chavez@dealertrend.com>
2013-01-07 14:50:45 -0800

    Bug: Admin Options Page - When in a new year, system looks to the next year for data. If data is not available, the call returns nothing, which caused the array_merge to fail. Made sure all arrays were set before doing the array_merge.

--- application/views/options/page.php
+++ application/views/options/page.php
@@ -161,9 +161,13 @@ class Options_Page {
 		$make_data[ $current_year ] = json_decode( $make_data[ $current_year ][ 'body' ] );
 		$make_data[ $next_year ] = $this->vehicle_reference_system->get_makes()->please( array( 'year' => $next_year ) );
 		$make_data[ $next_year ] = json_decode( $make_data[ $next_year ][ 'body' ] );
-		//Merge vehicle data so it displays as one array
-		$make_data = array_merge( $make_data[ $next_year ] , $make_data[ $current_year ] , $make_data[ $last_year ] );
-		//Returns merged array data
+
+		$make_data[ $last_year ] = is_array( $make_data[ $last_year ] ) ? $make_data[ $last_year ] : array();
+		$make_data[ $current_year ] = is_array( $make_data[ $current_year ] ) ? $make_data[ $current_year ] : array();
+		$make_data[ $next_year ] = is_array( $make_data[ $next_year ] ) ? $make_data[ $next_year ] : array();
+
+		$make_data = array_merge( $make_data[ $next_year ], $make_data[ $current_year ] , $make_data[ $last_year ] );
+
 		return $make_data;
 	}
 
