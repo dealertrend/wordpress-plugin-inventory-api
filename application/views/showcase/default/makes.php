04438ba23c4fc79a158a69a23e4f7829300104ea
cloudxero <jorge.chavez@dealertrend.com>
2013-01-08 14:17:18 -0800

    Bug: Fixed showcase make issue, involving no new year data (2014) being available.

--- application/views/showcase/default/makes.php
+++ application/views/showcase/default/makes.php
@@ -11,6 +11,10 @@
 	$make_data[ $next_year ] = $vehicle_reference_system->get_makes()->please( array( 'year' => $next_year ) );
 	$make_data[ $next_year ] = isset( $make_data[ $next_year ][ 'body' ] ) ? json_decode( $make_data[ $next_year ][ 'body' ] ) : NULL;
 
+	$make_data[ $last_year ] = is_array( $make_data[ $last_year ] ) ? $make_data[ $last_year ] : array();
+	$make_data[ $current_year ] = is_array( $make_data[ $current_year ] ) ? $make_data[ $current_year ] : array();
+	$make_data[ $next_year ] = is_array( $make_data[ $next_year ] ) ? $make_data[ $next_year ] : array();
+
 	$makes = array_merge( $make_data[ $next_year ] , $make_data[ $current_year ] , $make_data[ $last_year ] );
 
 	$selected_makes = $this->options[ 'vehicle_reference_system' ][ 'data' ][ 'makes' ];
